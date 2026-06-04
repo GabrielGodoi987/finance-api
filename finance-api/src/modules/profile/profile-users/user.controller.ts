3fc61f071d836a872362e82f94e0b25245bae992
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2026-06-04 18:31:31 -0300

    feat: add user controller wiring for update and deactivation

--- finance-api/src/modules/profile/profile-users/user.controller.ts
+++ finance-api/src/modules/profile/profile-users/user.controller.ts
@@ -1,19 +1,31 @@
-import { Body, Patch, Post } from '@nestjs/common';
+import { Body, Param, Patch, Post } from '@nestjs/common';
 import { ApplicationController } from '../../../commons/decorators/application/application.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUseCase } from './use-case/create.use-case';
+import { RequestDeactivationUseCase } from './use-case/reuqest-deactivate';
+import { UpdateUseCase } from './use-case/update.use-case';
 
 @ApplicationController('/client/users')
 export class UserController {
-  constructor(private readonly createUseCase: CreateUseCase) {}
+  constructor(
+    private readonly createUseCase: CreateUseCase,
+    private readonly updateUseCase: UpdateUseCase,
+    private readonly requestDeactivationUseCase: RequestDeactivationUseCase,
+  ) {}
+
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.createUseCase.execute(createUserDto);
   }
 
   @Patch()
-  update() {}
+  update(@Body() updateUserDto: UpdateUserDto) {
+    return this.updateUseCase.execute(updateUserDto);
+  }
 
-  @Post()
-  requestCancelationProfile() {}
+  @Post('/id')
+  requestDeactivation(@Param('id') id: string) {
+    return this.requestDeactivationUseCase.execute(id);
+  }
 }
